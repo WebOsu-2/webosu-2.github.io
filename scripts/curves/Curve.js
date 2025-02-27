8df864833e27928101a1d1ef66559c61990e0f8f
mostlime12195 <mostlime@outlook.com>
2025-02-27 18:35:58 +0000

    Automated minify of 87ed79f67292fd0d2d173f5b0da994439fb1f735

--- scripts/curves/Curve.js
+++ scripts/curves/Curve.js
@@ -1,9 +1,1 @@
-define([], function() {
-    function Curve(hitObject) {
-        this.hitObject = hitObject;
-    }
-    Curve.lerp = function lerp(a, b, t) {
-        return a * (1 - t) + b * t;
-    }
-    return Curve;
-});
+define([],(function(){function n(n){this.hitObject=n}return n.lerp=function(n,t,e){return n*(1-e)+t*e},n}));
